001c52b49554259cf5caef1decf23667bce7b6af
simon <zeus.mayu@gmail.com>
2020-10-12 18:37:09 +0800

    1.修改类名

--- src/test/java/zeus/simon/algos/algos01and10/AlgosIsPalindrome01Test.java
+++ src/test/java/zeus/simon/algos/algos01and10/AlgosIsPalindrome01Test.java
@@ -4,11 +4,11 @@ package zeus.simon.algos.algos01and10;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Algos01Test {
+public class AlgosIsPalindrome01Test {
 
     @Test
     public void isPalindromeTest() {
-        Algos01 v = new Algos01();
+        AlgosIsPalindrome01 v = new AlgosIsPalindrome01();
         Assert.assertEquals(true, v.isPalindrome(null));
         Assert.assertEquals(true,v.isPalindrome(""));
         Assert.assertEquals(true,v.isPalindrome(" race a E-car "));
